f93d9ee3621ef587256512aafb2cc8a9a4aebae6
Noname-29-lnin <128560496+Noname-29-lnin@users.noreply.github.com>
2025-12-20 00:40:14 +0700

    up cpp

---
+++ Questasim/FFT-8point-verilog-Implementation-/flist.f
@@ -0,0 +1,5 @@
+/home/noname/Documents/project_tiny/FFT_8Points/00_ref/00_code/FFT-8point-verilog-Implementation-/tb_fft_8point.sv
+/home/noname/Documents/project_tiny/FFT_8Points/00_ref/00_code/FFT-8point-verilog-Implementation-/fft_8point.v
+/home/noname/Documents/project_tiny/FFT_8Points/00_ref/00_code/FFT-8point-verilog-Implementation-/fp_adder_combined.v
+/home/noname/Documents/project_tiny/FFT_8Points/00_ref/00_code/FFT-8point-verilog-Implementation-/fp_multiplier.v
+/home/noname/Documents/project_tiny/FFT_8Points/00_ref/00_code/FFT-8point-verilog-Implementation-/butterfly.v
