7440d3713d53302012246252c95eae5b3286755a
Noname-29-lnin <128560496+Noname-29-lnin@users.noreply.github.com>
2026-01-06 19:55:43 +0700

    up

---
+++ Genus/Genus/flist.f
@@ -0,0 +1,28 @@
+./../../02_rtl/FPU_unit.sv
+./../../02_rtl/CLA_4bit.sv
+./../../02_rtl/CLA_8bit.sv
+./../../02_rtl/CLA_24bit.sv
+./../../02_rtl/CLA_28bit.sv
+./../../02_rtl/COMP_2bit.sv
+./../../02_rtl/COMP_4bit.sv
+./../../02_rtl/COMP_8bit.sv
+./../../02_rtl/COMP_28bit.sv
+// ./../../02_rtl/CSKA_4bit.sv
+// ./../../02_rtl/CSKA_28bit.sv
+./../../02_rtl/EXP_adjust.sv
+./../../02_rtl/EXP_rounding.sv
+./../../02_rtl/EXP_sub.sv
+./../../02_rtl/EXP_swap.sv
+./../../02_rtl/LOPD_4bit.sv
+./../../02_rtl/LOPD_8bit.sv
+./../../02_rtl/LOPD_16bit.sv
+./../../02_rtl/LOPD_24bit.sv
+./../../02_rtl/MAN_ALU.sv
+./../../02_rtl/MAN_swap.sv
+./../../02_rtl/NORMALIZATION_unit.sv
+./../../02_rtl/PSC_unit.sv
+./../../02_rtl/RND_add.sv
+./../../02_rtl/ROUNDING_unit.sv
+./../../02_rtl/SHF_left_28bit.sv
+./../../02_rtl/SHF_right_28bit.sv
+./../../02_rtl/SIGN_unit.sv
