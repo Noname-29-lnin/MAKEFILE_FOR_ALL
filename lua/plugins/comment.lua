93c6cbc745d068a5ab93a029c6f5edcd293451fd
My name is Noname <dong.nguyendai29k22@hcmut.edu.vn>
2026-01-30 16:22:19 +0700

    up

--- lua/plugins/comment.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "numToStr/Comment.nvim",
-    dependencies = { "JoosepAlviste/nvim-ts-context-commentstring" }, -- Đảm bảo Treesitter context plugin được tải trước
-    config = function()
-        require("Comment").setup({
-            pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-        })
-    end,
-}
